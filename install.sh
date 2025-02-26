d496723f7d5dfc3d9eef3a6e549d08bbe332d2d3
Jack M <jack.messerli-wallace@drake.edu>
2025-02-26 09:59:18 -0600

    Install script that hopefully works!

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Install Neovim
+if ! command_exists nvim; then
+    echo "Installing Neovim..."
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+    sudo rm -rf /opt/nvim
+    sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+else
+    echo "Neovim is already installed."
+fi
+
+# Update and install git if not installed
+if ! command_exists git; then
+    echo "Updating package list and installing git..."
+    sudo apt update && sudo apt install git -y
+else
+    echo "Git is already installed."
+fi
+
+# Install NVM (Node Version Manager)
+if [ -z "$NVM_DIR" ]; then
+    echo "Installing NVM..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+    echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.bashrc
+    echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> ~/.bashrc
+    echo '[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"' >> ~/.bashrc
+else
+    echo "NVM is already installed."
+fi
+
+# Install Node.js using NVM
+ if ! command_exists node; then
+     echo "Installing Node.js..."
+    # Source nvm directly
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+     nvm install node
+ else
+     echo "Node.js is already installed."
+ fi
+
+# Clone the jack-IDE repository
+ if [ ! -d "$HOME/.config/nvim" ]; then
+     echo "Cloning jack-IDE repository..."
+     git clone https://github.com/messerli-wallace/jack-IDE.git $HOME/.config/nvim
+ else
+     echo "jack-IDE repository is already cloned."
+ fi
+
+# Clone the packer.nvim repository
+if [ ! -d "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim" ]; then
+     echo "Cloning packer.nvim repository..."
+     git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+ else
+     echo "packer.nvim repository is already cloned."
+ fi
