1a79bbfe1e3901e8a06850a72a1a062ea58cf6c4
jack <jmesserli@coviance.com>
2025-11-13 11:53:10 -0600

    adjust conform

--- after/plugin/conform.lua
+++ after/plugin/conform.lua
@@ -1,16 +1,22 @@
 require("conform").setup({
-    formatters_by_ft = {
-        lua = { "stylua" },
-        -- Conform will run multiple formatters sequentially
-        python = { "ruff_format", "ruff_fix", "ruff_organize_imports" },
-        -- You can customize some of the format options for the filetype (:help conform.format)
-        rust = { "rustfmt", lsp_format = "fallback" },
-        -- Conform will run the first available formatter
-        javascript = { "prettierd", "prettier", stop_after_first = true },
-    },
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_format = "fallback",
-    },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "ruff_format", "ruff_fix", "ruff_organize_imports" },
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		rust = { "rustfmt", lsp_format = "fallback" },
+		-- Conform will run the first available formatter
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+	},
+	format_on_save = function(bufnr)
+		-- These options will be passed to conform.format()
+		if vim.g.disable_autoformat then
+			return
+		end
+		return {
+			timeout_ms = 500,
+			lsp_format = "fallback"
+		}
+	end
+	,
 })
